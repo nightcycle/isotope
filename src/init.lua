b1c1cad754f16addb4d6ef133fc1a97753b3e527
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-07-22 21:00:54 -0400

    Isotope explicit type

--- src/init.lua
+++ src/init.lua
@@ -11,11 +11,26 @@ export type Maid = Maid.Maid
 
 local Construct = require(script.Construct)
 
+export type Isotope = {
+	ClassName: string,
+	_isAlive: boolean?,
+	_Maid: Maid?,
+	_Fuse: Fuse?,
+	IsA: (self: Isotope, className: string) -> boolean,
+	Construct: (self: Isotope) -> nil,
+	Import: (self: Isotope, default: State | ValueState | any?, alt: any?) -> nil,
+	Destroy: ((self: Isotope) -> nil)?,
+	new: () -> Isotope,
+	[any]: any?,
+	__index: Isotope,
+}
+
+
 local Isotope = {}
 Isotope.ClassName = "Isotope"
 Isotope.__index = Isotope
 
-function Isotope:Destroy()
+function Isotope.Destroy(self: Isotope)
 	if self._isAlive ~= true then return end
 	self._isAlive = false
 	self._Maid:Destroy()
@@ -24,7 +39,7 @@ function Isotope:Destroy()
 	end
 end
 
-function Isotope:Import(stateOrVal, altValue): State
+function Isotope.Import(self: Isotope, stateOrVal, altValue): State
 	if type(stateOrVal) == "table" and stateOrVal.IsA and stateOrVal:IsA("State") then
 		return stateOrVal
 	elseif stateOrVal ~= nil then
@@ -38,7 +53,7 @@ function Isotope:Construct()
 	Construct(self)
 end
 
-function Isotope:IsA(className):boolean
+function Isotope.IsA(self: Isotope, className):boolean
 	if className == "Isotope" then return true end
 	if self.ClassName == className then return true end
 	local checkList = {}
@@ -57,14 +72,16 @@ function Isotope:IsA(className):boolean
 	return getClasses(self)
 end
 
-function Isotope.new()
-	local self = setmetatable({}, Isotope)
-	self._isAlive = true
-	self._Maid = Maid.new()
-	self._Fuse = Fusion.fuse(self._Maid)
+function Isotope.new(): Isotope
+	local maid: Maid = Maid.new()
+	local fuse: Fuse = Fusion.fuse(maid)
+	local self: any = {
+		_isAlive = true,
+		_Maid = maid,
+		_Fuse = fuse,
+	}
+	setmetatable(self, Isotope)
 	return self
 end
-
-export type Isotope = typeof(Isotope.new())
 
 return Isotope
